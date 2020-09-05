dcf930eeddf2023407198b40b96e104e2b5e4b9f
shrvn12120 <57077347+shrvn12120@users.noreply.github.com>
2020-09-05 16:54:05 +0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+function tryMe(){
+    var container = document.getElementById("container");
+    var hello = document.getElementById("hello");
+
+
+    container.style.display='block';
+    hello.style.display='block';
+}
